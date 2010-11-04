2af2f3ac817452a65de73eae6edd7eac0e6b0606
arfrank <arfrank@gmail.com>
2010-11-03 22:20:54 -0400

    Adding session library

--- handlers/main.py
+++ handlers/main.py
@@ -18,8 +18,11 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
 import os
+
+from libraries import gaesessions
 from models import accounts
 from helpers import application
+
 class MainHandler(webapp.RequestHandler):
 	def get(self):
 		path = os.path.join(os.path.dirname(__file__), '../templates/home.html')
