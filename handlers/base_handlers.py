592b6723a5743f90f0a63cc1c94d8983d0ac9315
arfrank <arfrank@gmail.com>
2010-11-07 00:14:06 -0400

    Adding error responses to match native twilio response

--- handlers/base_handlers.py
+++ handlers/base_handlers.py
@@ -1,6 +1,6 @@
 from google.appengine.ext import webapp
 
-from helpers import response, parameters, sid, authorization, xml, uris
+from helpers import response, parameters, sid, authorization, xml, uris, errors
 from decorators import authorization
 import math
 
@@ -8,34 +8,40 @@ class InstanceHandler(webapp.RequestHandler):
 	@authorization.authorize_request
 	def get(self,API_VERSION,ACCOUNT_SID, *args):
 		format = response.response_format( args[0] )
-		InstanceSid = args[0].split('.')[0]
-		Instance = self.ModelInstance.filter('Sid =',InstanceSid).filter('AccountSid = ',ACCOUNT_SID).get()
-		if Instance is not None and True:
-			response_data = Instance.get_dict()
-			response_data['ApiVersion'] = API_VERSION
-			response_data['Uri'] = self.request.path
-			if format == 'XML' or format == 'HTML':
-				response_data = xml.add_nodes(response_data,'')
-			self.response.out.write(response.format_response(response_data,format))
+		if 'GET' in self.AllowedMethods:
+			InstanceSid = args[0].split('.')[0]
+			Instance = self.ModelInstance.filter('Sid =',InstanceSid).filter('AccountSid = ',ACCOUNT_SID).get()
+			if Instance is not None:
+				response_data = Instance.get_dict()
+				response_data['ApiVersion'] = API_VERSION
+				response_data['Uri'] = self.request.path
+				if format == 'XML' or format == 'HTML':
+					response_data = xml.add_nodes(response_data,'')
+				self.response.out.write(response.format_response(response_data,format))
+			else:
+				self.response.out.write(response.format_response(errors.rest_error_response(400,"The requested resource was not found",format),format))
 		else:
-			self.error(400)
+			self.response.out.write(response.format_response(errors.rest_error_response(405,"The requested method is not allowed",format,20004,'http://www.twilio.com/docs/errors/20004'),format))
 
 	@authorization.authorize_request
 	def post(self, API_VERSION, ACCOUNT_SID, *args):
+		format = response.response_format( args[0] )
 		if 'POST' in self.AllowedMethods:
 			pass
 		else:
-			self.error(405)
+			self.response.out.write(response.format_response(errors.rest_error_response(405,"The requested method is not allowed",format,20004,'http://www.twilio.com/docs/errors/20004'),format))
 		
 	@authorization.authorize_request
 	def put(self, API_VERSION, ACCOUNT_SID, *args):
+		format = response.response_format( args[0] )
 		if 'PUT' in self.AllowedMethods:
 			pass
 		else:
-			self.error(405)
+			self.response.out.write(response.format_response(errors.rest_error_response(405,"The requested method is not allowed",format,20004,'http://www.twilio.com/docs/errors/20004'),format))
 
 	@authorization.authorize_request
 	def delete(self, API_VERSION, ACCOUNT_SID, *args):
+		format = response.response_format( args[0] )
 		if 'DELETE' in self.AllowedMethods:
 			format = response.response_format( args[0] )
 			InstanceSid = args[0].split('.')[0]
@@ -46,14 +52,14 @@ class InstanceHandler(webapp.RequestHandler):
 			else:
 				self.error(400)
 		else:
-			self.error(405)
+			self.response.out.write(response.format_response(errors.rest_error_response(405,"The requested method is not allowed",format,20004,'http://www.twilio.com/docs/errors/20004'),format))
 
 
 class ListHandler(webapp.RequestHandler):
 	@authorization.authorize_request
 	def get(self,API_VERSION,ACCOUNT_SID, *args):
+		format = response.response_format(self.request.path.split('/')[-1])
 		if 'GET' in self.AllowedMethods:
-			format = response.response_format(self.request.path.split('/')[-1])
 			if hasattr(self.AllowedFilters,'GET'):
 				for query_filter in self.AllowedFilters['GET']:
 					if query_filter[0] in self.request.arguments():
@@ -94,25 +100,31 @@ class ListHandler(webapp.RequestHandler):
 				response_data[self.ListName].append({self.ListModelName : instance.get_dict()})
 			self.response.out.write(response.format_response(response_data,format))
 		else:
-			self.error(400)
+			self.response.out.write(response.format_response(errors.rest_error_response(405,"The requested method is not allowed",format,20004,'http://www.twilio.com/docs/errors/20004'),format))
 
+	#Unlikely this will be used ever
 	@authorization.authorize_request
 	def post(self, API_VERSION, ACCOUNT_SID, *args):
+		format = response.response_format(self.request.path.split('/')[-1])
 		if 'POST' in self.AllowedMethods:
 			pass
 		else:
-			self.error(405)
+			self.response.out.write(response.format_response(errors.rest_error_response(405,"The requested method is not allowed",format,20004,'http://www.twilio.com/docs/errors/20004'),format))
 
+	#Unlikely this will be used ever
 	@authorization.authorize_request
 	def put(self, API_VERSION, ACCOUNT_SID, *args):
+		format = response.response_format(self.request.path.split('/')[-1])
 		if 'PUT' in self.AllowedMethods:
 			pass
 		else:
-			self.error(405)
+			self.response.out.write(response.format_response(errors.rest_error_response(405,"The requested method is not allowed",format,20004,'http://www.twilio.com/docs/errors/20004'),format))
 
+	#Unlikely this will be used ever
 	@authorization.authorize_request
 	def delete(self, API_VERSION, ACCOUNT_SID, *args):
+		format = response.response_format(self.request.path.split('/')[-1])
 		if 'DELETE' in self.AllowedMethods:
 			pass
 		else:
-			self.error(405)
+			self.response.out.write(response.format_response(errors.rest_error_response(405,"The requested method is not allowed",format,20004,'http://www.twilio.com/docs/errors/20004'),format))
