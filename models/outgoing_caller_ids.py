8f9ae85d4de81855be48ede390b3372e8d63a27b
arfrank <arfrank@gmail.com>
2010-11-13 17:10:26 -0500

    Adding models for incoming numbers and outgoing numbers

---
+++ models/outgoing_caller_ids.py
@@ -0,0 +1,12 @@
+from google.appengine.ext import db
+
+from models import base, phone_numbers
+
+from random import random
+
+from hashlib import sha256
+
+from helpers import parameters
+
+class Phone_Number(phone_numbers.Phone_Number):
+	pass
