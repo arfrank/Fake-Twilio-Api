7553fd33db95662b7551e0b9d59900c80fe89e55
arfrank <arfrank@gmail.com>
2010-11-14 21:43:35 -0500

    Adding parameters helper tests

--- unit_tests/parameters_helper_test.py
+++ unit_tests/parameters_helper_test.py
@@ -3,9 +3,11 @@ import logging
 
 from google.appengine.ext import db
 
+import models
+
 from helpers import parameters
 
-class ParametersParsePhoneNumbers(unittest.TestCase):
+class Parameters_Parse_Phone_Numbers(unittest.TestCase):
 	def setUp(self):
 		self.ValidPhoneNumber = '+12405559999'
 		self.TooShortPhoneNumber = '+1 222 222 222'
@@ -23,3 +25,33 @@ class ParametersParsePhoneNumbers(unittest.TestCase):
 	def test_Parse_Phone_Number_Failure_BadNumber(self):
 		phone_number, Valid = parameters.parse_phone_number(self.BadPhoneNumber)
 		self.assertFalse(Valid)
+
+class Parameters_Valid_To_Phone_Number(unittest.TestCase):
+	def setUp(self):
+		self.BadPhoneNumber = '+1999a123a23abc32'
+		self.Account  = models.accounts.Account.new(key_name='email@email.com',email='email@email.com',password='password')
+		self.Account.put()
+		self.PhoneNumber,Valid,TwilioCode, TwilioMsg = models.incoming_phone_numbers.Incoming_Phone_Number.new(PhoneNumber = '+13015559999', request = None, AccountSid = self.Account.Sid)
+		self.OutgoingCallerId, Valid, TwilioCode, TwilioMsg = models.outgoing_caller_ids.Outgoing_Caller_Id.new(PhoneNumber = '+13015556666', request = None, AccountSid = self.Account.Sid)
+		db.put([self.PhoneNumber, self.OutgoingCallerId])
+		
+	def test_To_Phone_Number_Success(self):
+		Valid, TwilioCode, TwilioMsg = parameters.valid_to_phone_number(self.PhoneNumber.PhoneNumber)
+		self.assertTrue(Valid)
+
+	def test_To_Phone_Number_Failure_Blank_Required(self):
+		Valid, TwilioCode, TwilioMsg = parameters.valid_to_phone_number('',True)
+		self.assertFalse(Valid)
+
+	def test_To_Phone_Number_Failure_Bad_Number(self):
+		Valid, TwilioCode, TwilioMsg = parameters.valid_to_phone_number(self.BadPhoneNumber,True)
+		self.assertFalse(Valid)
+		
+class Parameters_Valid_From_Phone_Number(unittest.TestCase):
+	def setUp(self):
+		self.BadPhoneNumber = '+1999a123a23abc32'
+		self.Account  = models.accounts.Account.new(key_name='email@email.com',email='email@email.com',password='password')
+		self.Account.put()
+		self.PhoneNumber,Valid,TwilioCode, TwilioMsg = models.incoming_phone_numbers.Incoming_Phone_Number.new(PhoneNumber = '+13015559999', request = None, AccountSid = self.Account.Sid)
+		self.OutgoingCallerId, Valid, TwilioCode, TwilioMsg = models.outgoing_caller_ids.Outgoing_Caller_Id.new(PhoneNumber = '+13015556666', request = None, AccountSid = self.Account.Sid)
+		db.put([self.PhoneNumber, self.OutgoingCallerId])
